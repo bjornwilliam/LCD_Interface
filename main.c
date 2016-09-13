b61865d239d14154c01f8403c0089e741cd27778
bjornwilliam <bjorn.omholt@gmail.com>
2016-09-13 19:18:53 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * LCD_proj.c
+ *
+ * Created: 9/13/2016 6:31:45 PM
+ * Author : Will
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
